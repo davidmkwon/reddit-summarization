106337a5edb186044365dfb9ade2fbf89f3f693e
davidmkwon <surval2001@gmail.com>
2020-12-03 23:27:33 -0500

    clean files

--- data/main.py
+++
@@ -1,8 +0,0 @@
-from convokit import Corpus, download
-
-# run if dataset is not downloaded yet:
-#corpus = Corpus(download('subreddit-travel'))
-
-data_path_home = '/Users/davidkwon/.convokit/downloads/subreddit-gatech'
-data_path_remote = '/home/dkwon49/.convokit/downloads/subreddit-travel'
-corpus = Corpus(data_path_remote)
